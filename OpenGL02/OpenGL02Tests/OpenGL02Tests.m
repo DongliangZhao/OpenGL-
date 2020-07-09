1403a595d939d16a4f186c2703dd576a9c1c68c8
赵东亮 <>
2020-07-09 10:45:04 +0800

    提交代码

---
+++ OpenGL02/OpenGL02Tests/OpenGL02Tests.m
@@ -0,0 +1,37 @@
+//
+//  OpenGL02Tests.m
+//  OpenGL02Tests
+//
+//  Created by 赵东亮 on 2020/7/9.
+//  Copyright © 2020 赵东亮. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OpenGL02Tests : XCTestCase
+
+@end
+
+@implementation OpenGL02Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
